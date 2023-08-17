6a8866b6bca8440f98bf338c8259637204fc66eb
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2023-08-17 15:52:34 +0300

    smth-commit

--- src/index.js
+++ src/index.js
@@ -1,75 +1,117 @@
 import axios from 'axios';
 
+// Constants for axios
 const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/';
-let currentPage = 1;
+const currentPage = 1;
 const recipesPerPage = 9;
 
+// DOM Elements
 const popularRecipesListRef = document.querySelector('.popular-recipes-list');
 const searchCategoriesListRef = document.querySelector(
   '.search-categories-list'
 );
+const allCategoriesBtnRef = document.querySelector(
+  '.search-all-categories-btn'
+);
+const recipeGalleryListRef = document.querySelector('.recipe-gallery-list');
 
-searchCategoriesListRef.addEventListener('click', e => {
-  e.preventDefault();
-  const searchQuery = e.target.value;
-  fetchRecipeByCategorie(searchQuery);
-});
+// Event Listeners
+searchCategoriesListRef.addEventListener('click', handleCategoryClick);
+allCategoriesBtnRef.addEventListener('click', handleAllCategoriesClick);
 
-function fetchRecipeByCategorie(categorie) {
+// Event Handlers
+function handleCategoryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
+  const searchQuery = event.target.value;
+  fetchRecipeByCategory(searchQuery);
+}
+
+function handleAllCategoriesClick(event) {
+  event.preventDefault();
+  fetchAllCategoriesRecipe();
+}
+
+// Fetching Functions
+function fetchRecipeByCategory(category) {
   return axios.get(
-    `${BASE_URL}recipes?page=${currentPage}&limit=${recipesPerPage}&category=${categorie}`
+    `${BASE_URL}recipes?page=${currentPage}&limit=${recipesPerPage}&category=${category}`
   );
 }
 
-const allCategoriesBtnRef = document.querySelector('.search-all-categories');
-allCategoriesBtnRef.addEventListener('click', e => {
-  e.preventDefault();
-  fetchAllCategoriesRecipe();
-});
-
 function fetchAllCategoriesRecipe() {
-  axios.get(`${BASE_URL}recipes?page=${currentPage}&limit=${recipesPerPage}`);
+  return axios.get(
+    `${BASE_URL}recipes?page=${currentPage}&limit=${recipesPerPage}`
+  );
 }
-
-fetchPopularRecipes().then(renderPopularRecipes).catch(console.warn);
 
 function fetchPopularRecipes() {
   return axios.get(`${BASE_URL}recipes/popular`);
 }
 
-function renderPopularRecipes({ data }) {
-  console.log(data);
-  const markup = data
+function fetchRecipesCategories() {
+  return axios.get(`${BASE_URL}categories`);
+}
+
+// Rendering Functions
+
+function renderGalleryMarkup({ data }) {
+  const markup = data.results
     .map(recipe => {
-      return `
-      <li class="popular-recipes-list-item">
-        <img class="popular-recipes-list-item-img" src="${recipe.preview}" alt="Food photo"/>
-        <div class="popular-recipes-list-item-info">
-          <h2 class="popular-recipes-list-item-title">${recipe.title}</h2>
-          <p class="popular-recipes-list-item-description">${recipe.description}</p>
-        </div>
-      </li>`;
+      return `<li><div class="gallery-list-item"></div></li>`;
     })
+    .join('');
+
+  recipeGalleryListRef.insertAdjacentHTML('beforeend', markup);
+}
+
+function renderPopularRecipes({ data }) {
+  const markup = data
+    .map(
+      recipe => `
+      <li class="popular-recipes-list-item">
+        <img class="popular-recipes-list-item-img" src="${
+          recipe.preview
+        }" alt="Food photo"/>
+        <div class="popular-recipes-list-item-info">
+          <h2 class="popular-recipes-list-item-title">${
+            recipe.title.length <= 14
+              ? recipe.title
+              : recipe.title.slice(0, 14) + '...'
+          }</h2>
+          <p class="popular-recipes-list-item-description">${
+            recipe.description.length <= 80
+              ? recipe.description
+              : recipe.description.slice(0, 80) + '...'
+          }</p>
+        </div>
+      </li>
+    `
+    )
     .join('');
 
   popularRecipesListRef.insertAdjacentHTML('beforeend', markup);
 }
 
-fetchRecipesCategories().then(renderRecipesCategories).catch(console.warn);
-
-function fetchRecipesCategories() {
-  return axios.get(`${BASE_URL}categories`);
-}
-
 function renderRecipesCategories({ data }) {
   const markup = data
-    .map(categorie => {
-      return `
+    .map(
+      category => `
       <li class="search-categories-item">
-         <button type="submit" class="search-categories-button" value="${categorie.name}">${categorie.name}</button>
-      </li>`;
-    })
+        <button type="submit" class="search-categories-button" value="${category.name}">${category.name}</button>
+      </li>
+    `
+    )
     .join('');
 
   searchCategoriesListRef.insertAdjacentHTML('beforeend', markup);
 }
+
+// Initialization
+fetchPopularRecipes().then(renderPopularRecipes).catch(console.warn);
+
+fetchRecipesCategories().then(renderRecipesCategories).catch(console.warn);
+
+fetchAllCategoriesRecipe().then(renderGalleryMarkup);
